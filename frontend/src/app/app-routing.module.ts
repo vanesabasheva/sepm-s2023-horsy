329f177ac1009acf70b2d812e28073702b165c04
Vanesa Basheva <e12119027@student.tuwien.ac.at>
2023-03-13 08:28:59 +0100

    [2, 24] add path for edit view

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,6 +1,9 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {HorseCreateEditComponent, HorseCreateEditMode} from './component/horse/horse-create-edit/horse-create-edit.component';
+import {
+  HorseCreateEditComponent,
+  HorseCreateEditMode
+} from './component/horse/horse-create-edit/horse-create-edit.component';
 import {HorseComponent} from './component/horse/horse.component';
 import {HorseDetailComponent} from './component/horse/horse-detail/horse-detail.component';
 
@@ -9,6 +12,7 @@ const routes: Routes = [
   {path: 'horses', children: [
     {path: '', component: HorseComponent},
     {path: 'create', component: HorseCreateEditComponent, data: {mode: HorseCreateEditMode.create}},
+    {path: ':id/edit', component:HorseCreateEditComponent, data: {mode: HorseCreateEditMode.edit}},
     {path: ':id', component: HorseDetailComponent},
   ]},
   {path: '**', redirectTo: 'horses'},
