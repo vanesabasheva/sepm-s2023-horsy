7846f54f5fa412a7055892eea62335fccd73aa62
Vanesa Basheva <e12119027@student.tuwien.ac.at>
2023-03-19 13:30:19 +0100

    [9, 24] add path and router link for Family Tree view

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import {HorseComponent} from './component/horse/horse.component';
 import {HorseDetailComponent} from './component/horse/horse-detail/horse-detail.component';
 import {OwnerComponent} from './component/owner/owner.component';
 import {OwnerCreateComponent} from './component/owner/owner-create/owner-create.component';
+import {HorseFamilyTreeComponent} from './component/horse/horse-family-tree/horse-family-tree.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'horses', pathMatch: 'full'},
@@ -16,6 +17,7 @@ const routes: Routes = [
     {path: 'create', component: HorseCreateEditComponent, data: {mode: HorseCreateEditMode.create}},
     {path: ':id/edit', component:HorseCreateEditComponent, data: {mode: HorseCreateEditMode.edit}},
     {path: ':id', component: HorseDetailComponent},
+    {path: ':id/familytree', component: HorseFamilyTreeComponent},
   ]},
   {path: 'owners', children: [
     {path: '', component: OwnerComponent},
