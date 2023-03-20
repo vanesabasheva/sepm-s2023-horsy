0072e37a6e43dac0e82bb47131559e9c995e241e
Vanesa Basheva <e12119027@student.tuwien.ac.at>
2023-03-20 09:23:34 +0100

    [13] add 2 negative tests in service layer for non-existing horses

--- backend/src/test/java/at/ac/tuwien/sepm/assignment/individual/service/HorseServiceTest.java
+++ backend/src/test/java/at/ac/tuwien/sepm/assignment/individual/service/HorseServiceTest.java
@@ -1,15 +1,20 @@
 package at.ac.tuwien.sepm.assignment.individual.service;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.tuple;
-
 import at.ac.tuwien.sepm.assignment.individual.dto.HorseListDto;
+import at.ac.tuwien.sepm.assignment.individual.exception.NotFoundException;
+import at.ac.tuwien.sepm.assignment.individual.exception.ServiceException;
 import at.ac.tuwien.sepm.assignment.individual.type.Sex;
-import java.util.List;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ActiveProfiles({"test", "datagen"}) // enable "test" spring profile during test execution in order to pick up configuration from application-test.yml
 @SpringBootTest
@@ -19,12 +24,27 @@ public class HorseServiceTest {
   HorseService horseService;
 
   @Test
-  public void getAllReturnsAllStoredHorses() {
+  public void getAllReturnsAllStoredHorses() throws ServiceException {
     List<HorseListDto> horses = horseService.allHorses()
         .toList();
-    assertThat(horses.size()).isGreaterThanOrEqualTo(1); // TODO adapt to exact number of elements in test data later
+    assertThat(horses.size()).isGreaterThanOrEqualTo(10);
     assertThat(horses)
         .map(HorseListDto::id, HorseListDto::sex)
         .contains(tuple(-1L, Sex.FEMALE));
   }
+
+  @Test
+  @DisplayName("Getting a non - existing horse throws NotFoundException")
+  public void getByIdOfNotExistingHorse() {
+    assertThrows(NotFoundException.class, () ->
+        horseService.getById(1));
+  }
+
+  @Test
+  @DisplayName("Deleting a non - existing horse throws NotFoundException")
+  public void deleteNotExistingHorse() {
+    assertThrows(NotFoundException.class,
+        () -> horseService.delete(42L));
+  }
+
 }
