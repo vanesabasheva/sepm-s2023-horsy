458ba7184af3807bf4037fb6b0a78c125dea189b
Vanesa Basheva <e12119027@student.tuwien.ac.at>
2023-03-27 19:13:49 +0200

    [28] add relations to test data for family tree

--- backend/src/main/resources/sql/insertData.sql
+++ backend/src/main/resources/sql/insertData.sql
@@ -22,15 +22,15 @@ VALUES (-1, 'Maverick', 'Radcliffe', null),
        (-9, 'Phoenix', 'Fire', 'phoenix.fire@example.com'),
        (-10, 'Ryder', 'Blaze', null);
 
-INSERT INTO horse (id, name, description, date_of_birth, sex)
-VALUES (-1, 'Wendy', 'The famous one!', '2012-12-12', 'FEMALE'),
-       (-2, 'Candy', 'The sweet one!', '2020-10-10', 'MALE'),
-       (-3, 'Sandy', 'The rough one!', '1976-03-03', 'FEMALE'),
-       (-4, 'Brandy', 'The adult!', '1977-10-10', 'MALE'),
-       (-5, 'Mandy', 'The edgy one!', '2000-09-09', 'FEMALE'),
-       (-6, 'Randy', null , '1970-04-12', 'MALE'),
-       (-7, 'Gwendy', 'The flamboyant one!', '2010-02-01', 'FEMALE'),
-       (-8, 'Handy', 'The handy one!', '2005-02-12', 'MALE'),
-       (-9, 'Bendy', 'The flexible one!', '2013-01-01', 'FEMALE'),
-       (-10, 'Pandy', 'The cutest one!', '2022-10-10', 'MALE');
+INSERT INTO horse (id, name, description, date_of_birth, sex, owner_id, mother_id, father_id)
+VALUES (-1, 'Wendy', 'The famous one!', '2012-12-12', 'FEMALE', null, null, null),
+       (-2, 'Candy', 'The sweet one!', '2020-10-10', 'MALE', -1, null, null),
+       (-3, 'Sandy', 'The rough one!', '1976-03-03', 'FEMALE', null, null, null),
+       (-4, 'Brandy', 'The adult!', '1977-10-10', 'MALE', -2, null, null),
+       (-5, 'Mandy', 'The edgy one!', '2000-09-09', 'FEMALE', -3, -3, -4),
+       (-6, 'Randy', null, '1970-04-12', 'MALE', -1, null, null),
+       (-7, 'Gwendy', 'The flamboyant one!', '2010-02-01', 'FEMALE', -10, -5, null),
+       (-8, 'Handy', 'The handy one!', '2005-02-12', 'MALE', null, null, -6),
+       (-9, 'Bendy', 'The flexible one!', '2013-01-01', 'FEMALE', null, -7, null),
+       (-10, 'Pandy', 'The cutest one!', '2022-10-10', 'MALE', -10, -7, -8);
 ;
